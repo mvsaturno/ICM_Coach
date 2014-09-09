0234d789884547aa62fe9c81e7c7a16c9980111c
mvsaturno <mv_saturno@yahoo.com.br>
2014-09-08 23:49:21 -0300

    Começando PFEquity

---
+++ www/js/hands_eval.js
@@ -0,0 +1,12 @@
+/*
+Sintaxe para chamar:
+equity = preFlopEquity(0,11,32,33); //A7c vs 78s
+retornar o número de vitórias, derrotas e empates para definir a porcentagem. 
+Repetir isso dentro de um loop até completar o range do(s) vilão(ões).
+
+Ou melhor, passar um array de mãos. Um objeto JS contendo as mãos do range.
+*/
+
+function preFlopEquity(hand1, hand2){
+
+}
